010c5b95ad5cfac3792a9bb537125b3e296eaed4
hoverwinter <hoverwinter@gmail.com>
2016-09-05 15:20:11 +0800

    refresh package

--- init.d/init-package.el
+++ init.d/init-package.el
@@ -1,13 +1,17 @@
 (require 'package)
-(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+; (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
+; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 (defun require-package (package)
   (if (package-installed-p package)
       t
-    (package-install package)))
+    (if (assoc pakcage package-archive-contents)
+	(package-install package)
+      (progn
+	(package-refresh-contents)
+	(package-install package)))))
 
 
 (provide 'init-package)
--- init.el
+++ init.el
@@ -24,4 +24,17 @@
 (require 'init-ace-jump)
 (require 'init-gnuplot)
 (require 'init-haskell)
-(require 'init-elpy)
+;(require 'init-elpy)
+(put 'narrow-to-region 'disabled nil)
+;(require 'kys)
+;(setq kys-directory "~/.emacs.d/kys")
+;(kys-init)
+
+(global-set-key "\M-sc" 'kys-session-commit)
+(global-set-key "\M-si" 'kys-session-init)
+(global-set-key "\M-sr" 'kys-session-reset)
+(global-set-key "\M-sd" 'kys-session-delete)
+(global-set-key "\M-sl" 'kys-session-list)
+(global-set-key "\M-sk" 'kys-session-kill)
+(global-set-key "\M-ss" 'kys-session)
+
